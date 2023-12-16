2ca1765aa5ad284d02aca6d216cc821a7cf6c362
Peter Camejo <thealex123@gmail.com>
2023-12-16 01:10:57 -0500

    sllight edits to contact page

--- components/sections/ContactSection.js
+++ components/sections/ContactSection.js
@@ -15,7 +15,7 @@ const ContactSection = () => {
              w="100%" 
              backgroundColor={useColorModeValue('#f58c00', '#03002e' )}
         >
-            <Container mb={8}>
+            <Container maxW='container.md' mb={8} centerContent>
                 <Heading as="h1" mb={5}>Thanks for stopping by!</Heading>
                 <Text fontSize={{base:"20px", md:"25px"}}>If you are interested in working with or hiring me, or even just in giving some feedback, feel free to contact me on social media or email me at pa.camejo@gmail.com!</Text>
             </Container>
